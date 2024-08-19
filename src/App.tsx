9f2a767091bb9e7ba95472fe3013f063a6193675
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-08-19 19:45:25 +0200

    add mobile styles for header

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,15 @@ import features from "./data/features";
 function App() {
   return (
     <>
-      <header>
-        <h1 className="">Reliable, efficient delivery</h1>
-        <h2 className="">Powered by Technology</h2>
-        <p className="">
+      <header className="mx-8 my-20 flex flex-col items-center">
+        <h1 className="text-2xl font-extralight">Reliable, efficient delivery</h1>
+        <h2 className="mb-4 text-2xl font-semibold">Powered by Technology</h2>
+        <p className="text-center text-sm opacity-50">
           Our Artificial Intelligence powered tools use millions of project data
           points to ensure that your project is successful
         </p>
       </header>
-      <section className="">
+      <section className="mx-8">
         {features.map((feature) => {
           return (
             <article className="" key={feature.name}>
