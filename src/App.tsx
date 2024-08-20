898175b27ec8b2046113531af1fe081ae8f2dcab
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-08-20 12:51:16 +0200

    refactor: move Features to separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,6 @@
-import features from "./data/features";
+import { Features } from "./components/Features";
 
 function App() {
-  const bgStyle = [
-    "bg-[#44D3D2] ",
-    "bg-[#EA5454]",
-    "bg-[#FCAE4A]",
-    "bg-[#549EF2]",
-  ];
-
   return (
     <>
       <header className="mx-8 mb-16 mt-20 flex flex-col items-center">
@@ -22,32 +15,7 @@ function App() {
           points to ensure that your project is successful
         </p>
       </header>
-      <section className="max-w-[70rem] mx-8 lg:mx-auto grid gap-6 lg:gap-7 lg:grid-cols-3 lg:grid-rows-2">
-        {features.map((feature, index) => {
-          return (
-            <article
-              className={`h-[13.875rem] flex flex-col bg-white rounded-lg shadow-feature overflow-hidden odd:lg:row-span-2 odd:self-center`}
-              key={feature.name}
-            >
-              <div className={`h-1 ${bgStyle[index]}`}></div>
-              <div className="p-7 flex flex-col justify-between grow">
-                <header>
-                  <h1 className="text-xl font-medium">{feature.name}</h1>
-                  <p className="text-xs opacity-50 leading-relaxed">
-                    {feature.description}
-                  </p>
-                </header>
-                <img
-                  width="57px"
-                  src={feature.icon_url}
-                  alt=""
-                  className="self-end"
-                />
-              </div>
-            </article>
-          );
-        })}
-      </section>
+      <Features />
     </>
   );
 }
---
+++ src/components/Features.tsx
@@ -0,0 +1,39 @@
+import features from "../data/features";
+
+const bgStyle = [
+  "bg-[#44D3D2] ",
+  "bg-[#EA5454]",
+  "bg-[#FCAE4A]",
+  "bg-[#549EF2]",
+];
+
+export function Features() {
+  return (
+    <section className="max-w-[70rem] mx-8 lg:mx-auto grid gap-6 lg:gap-7 lg:grid-cols-3 lg:grid-rows-2">
+      {features.map((feature, index) => {
+        return (
+          <article
+            className={`h-[13.875rem] flex flex-col bg-white rounded-lg shadow-feature overflow-hidden odd:lg:row-span-2 odd:self-center`}
+            key={feature.name}
+          >
+            <div className={`h-1 ${bgStyle[index]}`}></div>
+            <div className="p-7 flex flex-col justify-between grow">
+              <header>
+                <h1 className="text-xl font-medium">{feature.name}</h1>
+                <p className="text-xs opacity-50 leading-relaxed">
+                  {feature.description}
+                </p>
+              </header>
+              <img
+                width="57px"
+                src={feature.icon_url}
+                alt=""
+                className="self-end"
+              />
+            </div>
+          </article>
+        );
+      })}
+    </section>
+  );
+}
